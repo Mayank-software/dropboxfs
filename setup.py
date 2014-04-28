d07220f4ed4406687fe9dc1557c6dda4942faa65
Miguel Branco <miguel@raw-labs.com>
2014-04-29 00:52:18 +0200

    Add packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+  name='dropboxfs',
+  version='0.0.0',
+  author='Miguel Branco',
+  license='Postgresql',
+  packages=['dropboxfs'],
+  scripts=['dropboxfs/dropboxfs']
+)
+
